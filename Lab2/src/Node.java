270975ee10f1855b6efa202fe6ce3a2d93f06501
brandonabustamante <brandonabus1995@gmail.com>
2021-08-04 03:16:25 -0600

    Node.java created

---
+++ Lab2/src/Node.java
@@ -0,0 +1,18 @@
+
+public class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		next = null;
+	}
+
+	public void append(int data) {
+		Node current = this;
+		while (current.next != null) {
+			current = current.next;
+		}
+		current.next = new Node(data);
+	}
+}
